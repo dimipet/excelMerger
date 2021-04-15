0e8a62696ed07b9622bb5ffb4d4e36afae5afe80
dimipet <dimi.petridis@gmail.com>
2021-04-15 10:03:06 +0300

    application.properties.file convention: either read repository's application.properties.sample.xml OR edit global properties and assign specific file under application application.properties.file minor code changed to check if application.properties.file= is set in global.properties and use the path to file set or use provided application.properties.sample.xml

--- src/main/java/com/dimipet/excelmerger/App.java
+++ src/main/java/com/dimipet/excelmerger/App.java
@@ -1,6 +1,9 @@
 package com.dimipet.excelmerger;
 
 import java.io.File;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -11,25 +14,36 @@ import com.dimipet.excelmerger.controller.XLSXFileControllerImpl;
 import com.dimipet.excelmerger.properties.ExcelFiles;
 import com.dimipet.excelmerger.properties.ExcelFiles.InputFile;
 import com.dimipet.excelmerger.util.AppProperties;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class App {
 
     private static final Logger logger = Logger.getLogger( App.class.getName() ); 
     
+    public static final String SAMPLE_APP_PROPEPTIES_FILE = "application.properties.sample.xml";
+    
     public static void main(String[] args) {
         Properties prop =  AppProperties.getInstance();
-        logger.log(Level.INFO, "\n\n\n");
-        
 
         XLSXFileController xcontoller = new XLSXFileControllerImpl();
 
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(ExcelFiles.class);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            ExcelFiles excelFiles = (ExcelFiles) unmarshaller.unmarshal(new File(prop.getProperty("application.properties.file")));
+            
+            
+            // if not exists user's app properties file use the sample
+            String appPropFileVal = null;
+            if(prop.containsKey("application.properties.file")) {
+            	appPropFileVal = prop.getProperty("application.properties.file");
+            } else {
+            	appPropFileVal = App.SAMPLE_APP_PROPEPTIES_FILE;
+            }
+
+            logger.log(Level.INFO,"# using properties file : " + appPropFileVal);
+            
+            File appPropFile = new File(appPropFileVal); 
+            ExcelFiles excelFiles = (ExcelFiles) unmarshaller.unmarshal(appPropFile);
 
             xcontoller.prepareFile(
                     excelFiles.getOutputFile().getPath(),
@@ -38,10 +52,10 @@ public class App {
             );          
 
             for (InputFile inputFile : excelFiles.getInputFile()) {
-                logger.log(Level.INFO, "\n\n\n" );
-                logger.log(Level.INFO, inputFile.getPath() + " : parsing ..." );
+                logger.log(Level.INFO,"\n\n\n" );
+                logger.log(Level.INFO,inputFile.getPath() + " : parsing ..." );
                 
-                logger.log(Level.INFO, inputFile.getPath() + " : parsing header ..." );
+                logger.log(Level.INFO,inputFile.getPath() + " : parsing header ..." );
                 if (inputFile.getHeader().isParse()) {
                     xcontoller.mergeHeader(
                             inputFile.getPath(),
@@ -54,7 +68,7 @@ public class App {
                     );
                 }   
                 
-                logger.log(Level.INFO, inputFile.getPath() + " : parsing contents ..." );
+                logger.log(Level.INFO,inputFile.getPath() + " : parsing contents ..." );
                 xcontoller.mergeContent(
                         inputFile.getPath(),
                         inputFile.getWorkbook(),
--- src/main/java/com/dimipet/excelmerger/properties/XMLPropertiesValidator.java
+++ src/main/java/com/dimipet/excelmerger/properties/XMLPropertiesValidator.java
@@ -1,5 +1,6 @@
 package com.dimipet.excelmerger.properties;
 
+import com.dimipet.excelmerger.App;
 import com.dimipet.excelmerger.util.AppProperties;
 import com.dimipet.excelmerger.util.XMLValidator;
 import java.util.Properties;
@@ -11,7 +12,14 @@ public class XMLPropertiesValidator {
     public boolean validate(){
         Properties prop = AppProperties.getInstance();
         XMLValidator XMLValidator = new XMLValidator();
-        return XMLValidator.validate(prop.getProperty("application.properties.file"), SCHEMA_FILE); 
+        
+        String appPropFileVal = null;
+        if(prop.containsKey("application.properties.file")) {
+        	appPropFileVal = prop.getProperty("application.properties.file");
+        } else {
+        	appPropFileVal = App.SAMPLE_APP_PROPEPTIES_FILE;
+        }
+        return XMLValidator.validate(appPropFileVal, SCHEMA_FILE); 
     }
     
     
